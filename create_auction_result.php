579831da0ef9e45889cfba6cb3427664e240756b
Wenyong <wlai2436@gmail.com>
2021-11-05 09:50:16 +0800

    implement create auction, correct some Auction column settings, add column Title to Auction to make it fit the pre-given auction form.

--- create_auction_result.php
+++ create_auction_result.php
@@ -2,29 +2,37 @@
 
 <div class="container my-5">
 
+
 <?php
+    function printnl($text)
+    {
+        echo nl2br("$text\n");
+    }
 
-// This function takes the form data and adds the new auction to the database.
+    $SERVER = "localhost";
+    $USER = getenv('DBUSR') ?: "usr";
+    $PASS = getenv('DBPWD') ?: "pwd";
+    $DB = getenv('DB') ?: "db";
+    $TABLE = "Auction";
 
-/* TODO #1: Connect to MySQL database (perhaps by requiring a file that
-            already does this). */
+    $conn = new mysqli($SERVER, $USER, $PASS);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $conn->select_db($DB);
 
+    $sql = "INSERT INTO " . $TABLE ."(title, reservePrice, startingPrice, 
+    itemDescription, itemCat, endDate) VALUES (?, ?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    if(!$stmt) die("Statement prepare failed: " . $conn->error);
 
-/* TODO #2: Extract form data into variables. Because the form was a 'post'
-            form, its data can be accessed via $POST['auctionTitle'], 
-            $POST['auctionDetails'], etc. Perform checking on the data to
-            make sure it can be inserted into the database. If there is an
-            issue, give some semi-helpful feedback to user. */
+    $stmt->bind_param("siisss", $_POST['auctionTitle'], $_POST['auctionReservePrice'],
+    $_POST['auctionStartPrice'], $_POST['auctionDetails'], $_POST['auctionCategory'],
+    $_POST['auctionEndDate']);
+    if(!$stmt->execute()) die("Execution failed: " . $stmt->error);
 
-
-/* TODO #3: If everything looks good, make the appropriate call to insert
-            data into the database. */
-            
-
-// If all is successful, let user know.
-echo('<div class="text-center">Auction successfully created! <a href="FIXME">View your new listing.</a></div>');
-
-
+    // Successful
+    echo('<div class="text-center">Auction successfully created! <a href="FIXME">View your new listing.</a></div>');
 ?>
 
 </div>
