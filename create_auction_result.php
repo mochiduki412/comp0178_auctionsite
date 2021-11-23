7e9e2c4a1451543995fc8281f26e04fcb4ff39db
Wenyong <wlai2436@gmail.com>
2021-11-23 19:24:33 +0800

    add support to manage crediting bid winner, and update db accordingly. Add serveral cur bidprice, cur bidder and status to Auction to support better query and functionality. Move sql dumps to folder now.

--- create_auction_result.php
+++ create_auction_result.php
@@ -3,23 +3,36 @@
 
 <div class="container my-5">
 <?php
+    $ACTIVE = 1;
+
     session_start();
     if(!is_login()){
         redirect('index.php', 'Please log in before starting an auction.');
+    } elseif(!is_seller()){
+        redirect($_SERVER['HTTP_REFERER'] or 'index.php', 'You are not a seller.');
     } else{
         $seller = $_SESSION['user']; // the user id as identifier.
 
         $sql = "INSERT INTO Auction (title, sellerID, reservePrice, startingPrice, 
-                itemDescription, itemCat, endDate) VALUES (?, ?, ?, ?, ?, ?, ?)";
+                itemDescription, itemCat, endDate, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
         try{
-            prepare_bind_excecute($sql, "ssiisss", $_POST['auctionTitle'], $seller, $_POST['auctionReservePrice'], 
-                            $_POST['auctionStartPrice'], $_POST['auctionDetails'], $_POST['auctionCategory'], $_POST['auctionEndDate']);
+            prepare_bind_excecute(
+                $sql, "ssiisssi", 
+                $_POST['auctionTitle'], 
+                $seller, 
+                $_POST['auctionReservePrice'], 
+                $_POST['auctionStartPrice'], 
+                $_POST['auctionDetails'], 
+                $_POST['auctionCategory'], 
+                $_POST['auctionEndDate'],
+                $ACTIVE
+            );
         } catch(Exception $e){
-            echo('<div class="text-center">Internal error, please try again.</div>');
+            print_msg('Internal error, please try again.');
         }
 
         // auction created
-        echo('<div class="text-center">Auction is successfully created! <a href="mylistings.php">View your new listing.</a></div>');
+        print_msg('Auction is successfully created! <a href="mylistings.php">View your new listing.</a>');
         // redirect('create_auction.php'); // prevent refresh to re-post
     }
 ?>
--- db_utils.php
+++ db_utils.php
@@ -2,17 +2,17 @@
     /*
     * Wenyong: Will refactor after this file gathers some.
     */
-    $SERVER = "localhost";
-    $USER = getenv('DBUSR') ?: "db";
-    $PASS = getenv('DBPWD') ?: "db";
-    $DB = getenv('DB') ?: "db";
-
     function printnl($text)
     {
         echo nl2br("$text\n");
     }
 
     // ===== DB connection relateds =====
+    $SERVER = "localhost";
+    $USER = getenv('DBUSR') ?: "db";
+    $PASS = getenv('DBPWD') ?: "db";
+    $DB = getenv('DB') ?: "db";
+
     function get_conn(){
         global $SERVER, $USER, $PASS, $DB;
         $conn = new mysqli($SERVER, $USER, $PASS);
@@ -54,6 +54,13 @@
         return (isset($_SESSION['logged_in']) and $_SESSION['logged_in'] == true) ? true: false;
     }
 
+    function is_seller(){
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        return (isset($_SESSION['account_type']) and $_SESSION['account_type'] == 'seller') ? true: false;
+    }
+
    /*
     * Simple display for sql query results into html table.
     */
@@ -90,6 +97,9 @@
     }
 
     // ===== ORM-like relateds =====
+    $INACTIVE = 0;
+    $ACTIVE = 1;
+
     function get_max_bid_price_by_auction($auctionId){
         static $sql = "SELECT MAX(bidPrice) FROM `Bid` WHERE `auctionId`= ?";
         $res = prepare_bind_excecute($sql, 'i', $auctionId);
@@ -116,5 +126,11 @@
     function get_num_bid_by_auction($auctionId){
         static $sql = "SELECT COUNT(*) FROM `Bid` WHERE auctionId = ?";
         return (int) prepare_bind_excecute($sql, 'i', $auctionId)->fetch_row()[0];
+    }
+
+    function set_auction_inactive($auctionId){
+        global $INACTIVE;
+        static $sql = 'UPDATE Auction SET status = ? WHERE auctionId = ?';
+        return prepare_bind_excecute($sql, 'ii', $INACTIVE, $auctionId);
     }
 ?>
