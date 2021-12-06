c44cf054cd870d8dfb719ebe5302b076a44d7890
Wenyong <wlai2436@gmail.com>
2021-12-06 19:57:35 +0800

    add more impls on populate db

--- populate_db/populate_db.py
+++ populate_db/populate_db.py
@@ -1,6 +1,7 @@
 from random import choice, randint
 from datetime import date
 from os import getenv
+from uuid import uuid1
 
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy import create_engine, MetaData, Table, select
@@ -16,19 +17,19 @@ DB, SERV = getenv('DB') or "comp0178db", getenv('SERV') or "localhost"
 USER, PASS = getenv('USER') or "root", getenv('PASS') or ""
 engine = create_engine(f'mysql+pymysql://{USER}:{PASS}@{SERV}/{DB}', echo = False)
 metadata = MetaData()
-Base = automap_base()
-Base.prepare(engine, reflect = True)
 session = Session(engine)
 
 
 # reflect table to class
+Base = automap_base()
+Base.prepare(engine, reflect = True)
 Auction = Base.classes.Auction
 Bid = Base.classes.Bid
 User = Base.classes.User
 # print(Auction.__dict__.keys())
 
 
-# configure faker
+# faker custom data provider
 class UidProvider(BaseProvider):
     def __init__(self, generator):
         super().__init__(generator)
@@ -37,8 +38,15 @@ class UidProvider(BaseProvider):
     def get_uid(self):
         return choice(self._uids)
 
+class AuctionIdProvider(BaseProvider):
+    def __init__(self, generator):
+        super().__init__(generator)
+        self._auctionIds = tuple(map(lambda arr: arr[0], session.query(Auction.auctionId).all()))
+
+    def get_auctionId(self):
+        return choice(self._auctionIds)
+
 fake = Faker()
-fake.add_provider(UidProvider)
 fake.add_provider(VehicleProvider)
 
 
@@ -48,10 +56,28 @@ def commit_or_rollback(session):
         session.commit()
     except:
         session.rollback()
-        raise
+        pass
 
 
 # funcs to populate db
+def pop_users(cnt = 10):
+    i = 0
+    while i < cnt:
+        user = User(
+            userId = str(uuid1()),
+            firstName = fake.first_name(),
+            lastName = fake.last_name(),
+            email = fake.email(),
+            password = 'test',
+            type = choice(('buyer', 'seller',)),
+        )
+        session.add(user)
+        try:
+            commit_or_rollback(session)
+            i += 1
+        except IntegrityError: # happens to generate non-unique name/email etc.
+            raise
+
 def pop_auctions(cnt = 20):
     OPEN = 1
     i = 0 
@@ -76,14 +102,22 @@ def pop_auctions(cnt = 20):
             pass
 
 def pop_bids(cnt = 40):
-    pass
-
-def pop_users(cnt = 10):
-    pass
+    for i in range(cnt):
+        bid = Bid(
+            auctionId = fake.get_auctionId(),
+            bidderId = fake.get_uid(),
+            bidPrice = randint(100, 500),
+        )
+        session.add(bid)
+        commit_or_rollback(session)
 
 
 def run():
+    pop_users(5)
+    fake.add_provider(UidProvider)
     pop_auctions(10)
+    fake.add_provider(AuctionIdProvider) # to delay init for getting newly added auctionId. Can't think of better other ways for now.
+    pop_bids(20)
     print('Completed.')
 
 run()
